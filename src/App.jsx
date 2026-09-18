fda37a53eed7ec506e6539cc48ef749751be3fd1
JoaoACarboni <joaocarbonigomes00@gmail.com>
2026-09-17 21:07:25 -0300

    Aula click

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,22 @@ function App() {
       <section id="center">
         <div>
           <h1>Olá, meus amigos!</h1>
-           <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Clique aqui
-        </button>
-        <p>Você apertou no botão {count} vezes</p>
+            <button
+            type="button"
+            className="counter"
+            onClick={() => {setCount((count) => count + 1)
+
+              if (count === 9) {
+                alert('Você clicou 10 vezes!')
+                setCount(0)
+                alert('Sua contagem foi reiniciada')
+                alert("Seu processador está 109 graus celsius, você precisa desligar o computador imediatamente!")
+              }
+            }}
+          >
+            Clique aqui
+          </button>
+          <p>Você apertou no botão {count} vezes</p>
         </div>
       </section>
     </>
